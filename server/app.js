7cae5c390090e3b87ac1fd39c909fd62900af9cc
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-26 12:48:45 +0530

    Cleanup data

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
-if(!Posts.findOne({})) {
-  Posts.insert({
-    _id: "hello-world",
-    category: "meteor",
-    title: "Hello World",
-    content: "This is the hello world post"
-  });
-}
+Posts.remove({});
+Posts.insert({
+  _id: "hello-world",
+  category: "meteor",
+  title: "Hello World",
+  content: "This is the hello world post"
+});
