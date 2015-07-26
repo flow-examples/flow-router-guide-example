b9063d3dc2e2b49cf16c3d5154d87aec767beaeb
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-07-26 12:43:09 +0530

    clean data set

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
-if(!Posts.findOne()) {
-  var userId = Accounts.createUser({username: "the-owner", password: "password"});
-  Posts.insert({
-    _id: "hello-world",
-    title: "Hello World Post",
-    content: "This is the Hello World content",
-    owner: userId
-  });
-}
+Posts.remove({});
+Meteor.users.remove({});
+
+var userId = Accounts.createUser({username: "the-owner", password: "password"});
+Posts.insert({
+  _id: "hello-world",
+  title: "Hello World Post",
+  content: "This is the Hello World content",
+  owner: userId
+});
 
 Meteor.publish('singlePost', function(id) {
   check(id, String);
